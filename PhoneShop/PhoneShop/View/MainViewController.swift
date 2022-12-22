9ba984b08aff7e3379e6b2121a445b54e92d75f4
dmitry <dmitry.galay.91@gmail.com>
2022-12-22 16:09:06 +0300

    create project

---
+++ PhoneShop/PhoneShop/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  PhoneShop
+//
+//  Created by Dima on 22.12.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhoneShop/Pods/Target Support Files/Pods-PhoneShop/Pods-PhoneShop-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhoneShop : NSObject
+@end
+@implementation PodsDummy_Pods_PhoneShop
+@end
